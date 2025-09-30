96b4bae5095d92e419674f5e19d906d6719a0509
Tony Amende <tony.amende@gmail.com>
2025-09-30 21:17:20 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Steidl/',
+  base: '/BirkmayerAerospace_Webseite/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
