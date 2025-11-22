f74784d11d4aae64738a05897bdb2b14085640e9
Tony Amende <tony.amende@gmail.com>
2025-11-22 08:23:27 +0100

    update Logo

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
         <div className="bg-white/10 backdrop-blur-sm text-white">
           <div className="container mx-auto px-4 py-4 flex items-center justify-between">
             <img
-              src={`${import.meta.env.BASE_URL}logo_merch1.png`}
+              src={`${import.meta.env.BASE_URL}logo_merch5.png`}
               alt="Birkmayer Aerospace Logo"
               className="h-10"
             />
